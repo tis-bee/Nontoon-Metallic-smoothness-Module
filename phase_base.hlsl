b5d21a092972a1220d9abafffed2844ad637d1af
tis-bee <136098996+tis-bee@users.noreply.github.com>
2026-07-21 18:31:27 +0100

    Add files via upload

--- phase_base.hlsl
+++ phase_base.hlsl
@@ -1,13 +1,15 @@
 {
-    sd.normalMapWithRoughness = 1;
+    if(_MetRoughEnable){
+        sd.normalMapWithRoughness = 1;
 
-    half roughnessTex = SCSample(_RoughnessMap, sampler_BaseTexture, sd.uv).r;
-    half metallicTex = SCSample(_MetallicMap, sampler_BaseTexture, sd.uv).r;
+        half roughnessTex = SCSample(_RoughnessMap, sampler_BaseTexture, sd.uv).r;
+        half metallicTex = SCSample(_MetallicMap, sampler_BaseTexture, sd.uv).r;
 
-    half metallic = saturate(metallicTex * _MetallicMapIntensity);
+        half metallic = saturate(metallicTex * _MetallicMapIntensity);
 
 
-    sd.roughness -= saturate(_Roughness * roughnessTex * (_RoughnessMapIntensity * 10));
+        sd.roughness -= saturate(_Roughness * roughnessTex * (_RoughnessMapIntensity * 10));
 
-    sd.albedoAlpha.rgb = lerp(sd.albedoAlpha.rgb, sd.albedoAlpha.rgb * 0.15, metallic);
+        sd.albedoAlpha.rgb = lerp(sd.albedoAlpha.rgb, sd.albedoAlpha.rgb * 0.15, metallic);
+        }
 }
--- properties.hlsl
+++ properties.hlsl
@@ -1,12 +1,8 @@
-SC_uint(_EmissionEnable, 0, [SCInHeader][SCToggle][SCConstValue(1,pixel)], "", "")
+SC_uint(_MetRoughEnable, 0, [SCInHeader][SCToggle][SCConstValue(1,pixel)], "", "")
+SC_Box
+SC_Texture2D(_RoughnessMap, "white", [SCCache], "Roughness Map", "")
+SC_float(_RoughnessMapIntensity, 1, [SCRange(0, 1)], "Roughness Multiplier", "")
 
-SC_color(_EmissionColor, (1,1,1,1), [SCHDR], "Color", "")
-SC_float(_EmissionBoost, 1, [SCRange(0,1)], "Emission Boost", "")
-SC_uint(_EmissionUseBaseCol, 0, [SCToggle], "Use Base Color", "")
-
-SC_Texture2D(_EmissionTex, "black", [SCCache], "Emission Texture", "")
-SC_uint(_EmissionUseTex, 0, [SCToggle], "Use Texture", "")
-
-SC_uint(_EmissionMaskChannel, 3, [SCCache][SCMaskChannel], "__MaskChannel", "")
-SC_uint(_EmissionUseMask, 1, [SCToggle], "Use Mask", "")
-SC_uint(_EmissionUsePhaseAdd, 0, [SCToggle], "Additive Shading", "")
+SC_Texture2D(_MetallicMap, "white", [SCCache], "Metallic Map", "")
+SC_float(_MetallicMapIntensity, 1, [SCRange(0, 1)], "Metallic Multiplier", "")
+SC_BoxEnd
